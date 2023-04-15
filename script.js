3600fc0521dcf5f9c32334712cb94de1f60aa504
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-15 21:37:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function add_element () {
+   document.body.innerHTML = "DOM load success"
+}
+function myLoad()
+{
+   add_element();
+}
